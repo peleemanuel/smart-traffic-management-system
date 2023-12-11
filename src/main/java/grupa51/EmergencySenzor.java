1f0d6827158d458e5302771bc054076e505ca7f1
helenajullia <helenajulliapastore@yahoo.com>
2023-12-11 16:31:27 +0200

    Updated EmergencySenzor Class & Test with the needed requests

--- src/main/java/grupa51/EmergencySenzor.java
+++ src/main/java/grupa51/EmergencySenzor.java
@@ -1,14 +1,37 @@
 package grupa51;
 
+/**
+ * Clasa EmergencySenzor servește ca un detector pentru situații de urgență în cadrul unui sistem de control al semafoarelor.
+ * Această clasă este utilizată pentru a modifica comportamentul normal al semafoarelor,
+ * permițând vehiculelor de urgență, precum ambulanțele, să treacă prin intersecții rapid și în siguranță.
+ * Când senzorul de urgență este activat, el poate declanșa o schimbare a secvențelor de semafor pentru a acorda prioritate vehiculului de urgență,
+ * de exemplu, schimbând semaforul la verde pentru a permite ambulanței să treacă fără întârziere.
+ * Alte funcționalități pot include ajustarea timpilor de semafor în funcție de trafic și alte condiții dinamic detectate.
+ */
 public class EmergencySenzor {
-    private boolean emergency = false; // Initial state is not in emergency
+    private boolean emergency; // Starea senzorului de urgență
 
-    public boolean getStatus() {
-        return emergency;
+    /**
+     * Constructor pentru clasa EmergencySenzor care inițializează starea senzorului.
+     * @param initialStatus Starea inițială a senzorului de urgență, falsă dacă nu este activat.
+     */
+    public EmergencySenzor(boolean initialStatus) {
+        this.emergency = initialStatus;
     }
 
+    /**
+     * Returnează starea curentă a senzorului de urgență.
+     * @return boolean - Adevărat dacă senzorul este activat, fals în caz contrar.
+     */
+    public boolean getStatus() {
+        return this.emergency;
+    }
+
+    /**
+     * Setează starea senzorului de urgență.
+     * @param status Starea dorită pentru senzorul de urgență, adevărat pentru activat.
+     */
     public void setStatus(boolean status) {
-        emergency = status;
+        this.emergency = status;
     }
 }
-
--- src/test/java/EmergencySenzorTest.java
+++ src/test/java/EmergencySenzorTest.java
@@ -10,22 +10,26 @@ class EmergencySenzorTest {
 
     @BeforeEach
     void setUp() {
-        senzor = new EmergencySenzor();
+        // Inițializăm senzorul cu starea inițială false (nu în stare de urgență)
+        senzor = new EmergencySenzor(false);
     }
 
     @Test
     void testInitialCondition() {
+        // Verificăm condiția inițială, care ar trebui să fie false
         assertFalse(senzor.getStatus(), "Sensor should not be in emergency state initially");
     }
 
     @Test
     void testSetStatusTrue() {
+        // Setăm starea senzorului la true și verificăm
         senzor.setStatus(true);
         assertTrue(senzor.getStatus(), "Sensor status should be true after being set to true");
     }
 
     @Test
     void testSetStatusFalse() {
+        // Setăm starea senzorului la false și verificăm
         senzor.setStatus(false);
         assertFalse(senzor.getStatus(), "Sensor status should be false after being set to false");
     }
