f02a7ffff10e6539f4a6c55aaf33585dcc346d01
nistorcarina <carinanistor123321@gmail.com>
2023-12-12 22:51:01 +0200

    Added Memorie class

---
+++ src/test/java/MemorieTest.java
@@ -0,0 +1,71 @@
+import grupa51.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemorieTest {
+    private Memorie memorie;
+    private int[] debite;
+    private int[] coef;
+
+    @BeforeEach
+    public void setUp(){
+        debite = new int[] {10, 20, 30};
+        coef = new int[] {3, 4, 5};
+        memorie=new Memorie(debite,coef);
+    }
+
+    @DisplayName("Test GetTMIN")
+    @Test
+    public void testGetTMIN(){
+        assertEquals(Memorie.getTMIN(),20,"TMIN should be 20");
+    }
+
+    @DisplayName("Test GetTMIN")
+    @Test
+    public void testGetTMAX(){
+        assertEquals(Memorie.getTMAX(),40,"TMAX should be 40");
+    }
+
+    @DisplayName("Test GetTMED")
+    @Test
+    public void testGetTMED(){
+        assertEquals(Memorie.getTMED(),30,"TMED should be 30");
+    }
+
+    @DisplayName("Test GetTDIF")
+    @Test
+    public void testGetTDIF(){
+        assertEquals(Memorie.getTDIF(),20,"TDIF should be 20");
+    }
+
+    @DisplayName("Test GetTMIN")
+    @Test
+    public void testGetKINIT(){
+        assertEquals(Memorie.getKINIT(),1,"KINIT should be 20");
+    }
+
+    @Test
+    @DisplayName("Test GetSetDebite")
+    public void testGetSetDebite() {
+        assertArrayEquals(debite, memorie.getDebite(), "Debite array should match initial values");
+
+        int[] newDebite = new int[] {40, 50, 60};
+        memorie.setDebite(newDebite);
+        assertArrayEquals(newDebite, memorie.getDebite(), "Debite array should match new values");
+    }
+
+    @Test
+    @DisplayName("Test GetSetCoef")
+    public void testGetSetCoef() {
+        assertArrayEquals(coef, memorie.getCoef(), "Coef array should match initial values");
+
+        int[] newCoef = new int[] {6, 7, 8};
+        memorie.setCoef(newCoef);
+        assertArrayEquals(newCoef, memorie.getCoef(), "Coef array should match new values");
+    }
+
+
+}
